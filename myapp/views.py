b4e61eacc06e925157434ac2ed1bbc9932cd3cb3
mahabubr <mahabub.r753@gmail.com>
2023-01-08 00:09:34 +0600

    first commit

---
+++ myapp/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Feature
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+
+# Create your views here.
+
+
+def index(request):
+    features = Feature.objects.all()
+    return render(request, 'index.html', {'feature': features})
+
+
+def counter(request):
+    return render(request, 'counter.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, "Email Already Used")
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, "User Name Already Used")
+                return redirect('register')
+            else:
+                user = User.objects.create_user(
+                    username=username, email=email, password=password
+                )
+                user.save()
+                return redirect('login')
+        else:
+            messages.info(request, "Password Are Not To Same")
+            return redirect('register')
+    else:
+        return render(request, 'register.html')
